4f64f3acfaa4b581b6dcc1ab75ed90f847ac8367
Julien Arnould <Arnould_julien@outlook.fr>
2023-07-04 16:02:30 +0200

    first commit

---
+++ Resources/views/includes/footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<footer>
+  <div class="footer-links">
+    <a href="">Home</a>
+    <a href="">Invoices</a>
+    <a href="">Companies</a>
+    <a href="">Contact</a>
+    <a href="">Privacy Policy</a>
+  </div>
+
+  <div class="social-icons">
+    <a href="" target="_blank"><i class="fab fa-facebook"></i></a>
+    <a href="" target="_blank"><i class="fab fa-twitter"></i></a>
+    <a href="" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="" target="_blank"><i class="fab fa-linkedin"></i></a>
+  </div>
+  <div class="address">
+    Square des Martyrs, 6000 Charleroi
+  </div>
+  <div class="telephone">
+  (123) 456-7890
+  </div>
+  <div class="copyright">
+    &copy; 2023 BeCode. Tous droits réservés.
+  </div>
+  
+  
+</footer>
+</html>
