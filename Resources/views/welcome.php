857faf94e7ce4baddf03c205052b33a451fa0d0f
Anthony <denayeranthony1782@gmail.com>
2023-07-06 16:58:32 +0200

    first array joli

--- Resources/views/welcome.php
+++ Resources/views/welcome.php
@@ -1,6 +1,7 @@
   <?php
     include 'includes/header.php';
     include 'includes/slogan.php';
+    include 'includes/displayArray.php';
   ?>  
     <main id="main">
       <?php 
@@ -14,13 +15,13 @@
         // Accès autorisé pour invoices : id, ref, id_company, created_at, updated_at
 
         foreach($contacts as $contact){
-          echo "<li>$contact[name]</li>";
+          // echo "<li>$contact[name]</li>";
         }
 
         // Accès autorisé pour contacts : id, name, company_id, email, phone, created_at, updated_at
 
         foreach($companies as $company){
-          echo "<li>$company[name]</li>";
+          // echo "<li>$company[name]</li>";
         }
 
         // Accès autorisé pour companies : id, name, type_id, country, tva, created_at, updated_at
