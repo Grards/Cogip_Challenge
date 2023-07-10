6d3fefa1973185fa246201ec8787477e371e1f35
BillyGrind <denayeranthony623@gmail.com>
2023-07-10 10:25:27 +0200

    delete navbar.js and insert html in header.php

--- Resources/views/includes/header.php
+++ Resources/views/includes/header.php
@@ -10,7 +10,28 @@
     <title>Cogip - Welcome</title>
 </head>
 <body>
+ <!-- <div id="navbar-buttons">
+ <button type="button">Sign up</button>
+ <button type="button">Login</button>
+ </div> -->
 <div id="navbar-container">
     <nav id="navbar">
+    <div id="navbar-title">
+        <h2>COGIP</h2>
+    </div>
+    <div id="navbar-burger">
+        <span></span>
+    </div>
+  
+    <div id="navbar-menu">
+        <ul>
+            <li><a href="./">Home</a></li>
+            <li><a href="./invoices.php">Invoices</a></li>
+            <li><a href="./companies.php">Companies</a></li>
+            <li><a href="./contacts.php">Contacts</a></li>
+            <li><a href="#">Sign Up</a></li>
+            <li><a href="#">Login</a></li>
+        </ul>
+    </div>
     </nav>
 </div>
