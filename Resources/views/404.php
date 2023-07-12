1a55690dce3317465e15b0232d091f78f592e94f
Julien Arnould <Arnould_julien@outlook.fr>
2023-07-12 16:18:17 +0200

    404

--- Resources/views/404.php
+++ Resources/views/404.php
@@ -1,10 +1,24 @@
 <?php
-    include VIEWS.'includes/header.php';
-?>  
+   include VIEWS.'includes/header.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Erreur 404 - Page introuvable</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo BASE_URL.'assets/css/error_404.css'?>">
+    <link rel="stylesheet" type="text/css" href="../../scss/layout/_404.scss">
+</head>
+<body>
     <main id="main">
-        <img src="<?php echo BASE_URL.'assets/img/404.jpg'?>" alt="A robot who says 'error 404'" style="width:300px; height:300px;">
-        <p><a href="<?php echo BASE_URL?>">Return to home</a></p>
+        <img src="<?php echo BASE_URL.'assets/img/404.svg'?>" alt="A robot who says 'error 404'">
+        <div class="button">
+            <p><a href="<?php echo BASE_URL?>">Retour à l'accueil</a></p>
+        </div>
+
+
     </main>
+</body> 
+</html>
 <?php
     // include 'includes/footer.php';
 ?>
