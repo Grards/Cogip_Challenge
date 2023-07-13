b73d9eec3aaacd41a44eb8f5dd90f0511ce26ab9
Steve Grard <stevegrard1@gmail.com>
2023-07-13 14:36:13 +0200

    :construction: WIP for contacts img

--- Controllers/AdminsController.php
+++ Controllers/AdminsController.php
@@ -52,12 +52,43 @@ class AdminsController extends Controller
             if(isset($_POST['new-contact'])){
                 $contact_name = htmlspecialchars($_POST['new-contact__name']);
                 $contact_company = htmlspecialchars($_POST['new-contact__company']);
-                $contact_email = filter_var($_POST['new-contact__email'], FILTER_SANITIZE_EMAIL);
+                $contact_email = filter_var(htmlspecialchars($_POST['new-contact__email']), FILTER_SANITIZE_EMAIL);
                 $contact_phone = htmlspecialchars($_POST['new-contact__phone']);
                 $contact_created_at = date("Y/m/d h:m:s");
+                $contact_picture = "";
 
                 if(isset($_POST['new-contact__picture'])){
-                    // Picture treatment
+                    // File validation
+                    define("ALLOWED_SIZE", 2097152);    // CHANGE ALLOWED SIZE AS YOUR NEED
+                    define("SAVED_DIRECTORY", IMG."contacts/"); // CHANGE SAVED DIRECTORY AS YOUR NEED
+
+                    $allowed_extensions = array("jpeg", "jpg", "png"); // CHANGE allowed extension AS YOUR NEED
+
+                    if(isset($_FILES['new-contact__picture'])){
+                        $errors = array();
+                        
+                        $uploaded_file_name = $_FILES['new-contact__picture']['name'];
+                        $uploaded_file_size = $_FILES['new-contact__picture']['size'];
+                        $uploaded_file_tmp  = $_FILES['new-contact__picture']['tmp_name'];
+                        $uploaded_file_type = $_FILES['new-contact__picture']['type'];
+
+                        $file_compositions = explode('.', $uploaded_file_name);
+                        $file_ext = strtolower(end($file_compositions));
+                        
+                        $saved_file_name = $uploaded_file_name; // CHANGE FILE NAME AS YOUR NEED
+                        if(in_array($file_ext, $allowed_extensions) === false)
+                            $errors[] = 'Extension not allowed, please choose a JPEG or PNG file';
+
+                        if($uploaded_file_size > ALLOWED_SIZE)
+                            $errors[] = 'File size is too big';
+
+                        if(empty($errors) == true) { // if no error, uploaded image is valid
+                            move_uploaded_file($uploaded_file_tmp, SAVED_DIRECTORY . $saved_file_name);
+                            $contact_picture = $uploaded_file_name;
+                        } else {
+                            print_r($errors);
+                        }
+                    }
                 }
 
                 return $this->viewAdmin('treatment',[
@@ -65,7 +96,8 @@ class AdminsController extends Controller
                     "contact_company" => $contact_company,
                     "contact_email" => $contact_email,
                     "contact_phone" => $contact_phone,
-                    "contact_created_at" => $contact_created_at
+                    "contact_created_at" => $contact_created_at,
+                    "contact_picture" => $contact_picture
                 ]);
             }
         }
