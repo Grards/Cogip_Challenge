31aead20b460b5f7c0cd9a2b54041803682b19d8
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-07-13 16:33:05 +0200

    Column sorting functionnal after making a research

--- Resources/views/includes/pagination.php
+++ Resources/views/includes/pagination.php
@@ -3,23 +3,23 @@
         $link = BASE_URL.'contacts';
         
         if($currentPage > 1):  
-            echo "<a href=" . $link . "?page=" . $currentPage - 1 . "&search=" . $searchQuery . "&sort=" . $sortField . "&order=" . $sortOrder . " class='pagination__btn coin'>&lt;</a>";
+            echo "<a href=" . $link . "?page=" . $currentPage - 1 . "&search=" . $searchQuery . "&sort_field=" . $sortField . "&sort_order=" . $sortOrder . " class='pagination__btn coin'>&lt;</a>";
         endif;
 
         $pageNumber = 1;
         while($pageNumber <= $pages){
             if($pageNumber == 1 || $pageNumber == 2 || $pageNumber == $currentPage || $pageNumber == $pages-1 || $pageNumber == $pages){
-                echo $currentPage == $pageNumber ? "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort=" . $sortField . "&order=" . $sortOrder . " class='pagination__btn pagination__btn--current_page'>" . $pageNumber . "</a>" : "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort=" . $sortField . "&order=" . $sortOrder . " class='pagination__btn'>" . $pageNumber . "</a>";
+                echo $currentPage == $pageNumber ? "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort_field=" . $sortField . "&sort_order=" . $sortOrder . " class='pagination__btn pagination__btn--current_page'>" . $pageNumber . "</a>" : "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort_field=" . $sortField . "&sort_order=" . $sortOrder . " class='pagination__btn'>" . $pageNumber . "</a>";
             }else{
                 if(($pageNumber <= $pages-2 && $pageNumber == $currentPage+1) || ($pageNumber >= 3 && $pageNumber == $currentPage-1) || ($pageNumber == $currentPage+2) && ($currentPage == 1) || ($pageNumber == $currentPage-2) && ($currentPage == $pages)){
-                    echo "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort=" . $sortField . "&order=" . $sortOrder . " class=pagination__btn>...</a>";  
+                    echo "<a href=" . $link . "?page=" . $pageNumber . "&search=" . $searchQuery . "&sort_field=" . $sortField . "&sort_order=" . $sortOrder . " class=pagination__btn>...</a>";  
                 }
             }
             $pageNumber++;
         }
 
         if($currentPage < $pages): 
-            echo "<a href=" . $link . "?page=" . $currentPage + 1 . "&search=" . $searchQuery . " class='pagination__btn'>&gt;</a>";
+            echo "<a href=" . $link . "?page=" . $currentPage + 1 . "&search=" . $searchQuery . "&sort_field=" . $sortField . "&sort_order=" . $sortOrder .  " class='pagination__btn'>&gt;</a>";
         endif;
     echo "</section>"
 ?>
