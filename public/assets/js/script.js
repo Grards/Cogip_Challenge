3fe0233fad54f375886674ced2208a4d31300954
Julien Arnould <Arnould_julien@outlook.fr>
2023-07-14 13:57:45 +0200

    error display notification

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,9 @@
 import { burgerMenu } from "./burgerMenu.js";
+import { errorNotificationDisplay} from "./errorNotificationDisplay.js";
+
+errorNotificationDisplay();
+console.log("hello world");
 
 burgerMenu();
 console.log("hello");
+
