14cd1aade3538fa0b7edc7da1fded8e4496a1f74
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-07-14 10:59:24 +0200

    Join companies name to contacts table

--- Models/Contact.php
+++ Models/Contact.php
@@ -29,7 +29,8 @@ class Contact
     }
 
     public function getAllContacts() {
-        $query = "SELECT id, name, company_id, email, phone, created_at, updated_at FROM contacts";
+        $query = "SELECT id, contacts.name, company_id, email, phone, created_at, updated_at
+        FROM contacts";
         $statement = $this->db->prepare($query);
         $statement->execute();
 
@@ -37,7 +38,8 @@ class Contact
     }
 
     public function getCountOfContacts($searchQuery) {
-        $query = "SELECT COUNT(id) FROM contacts WHERE name LIKE :query ORDER BY name";
+        $query = "SELECT COUNT(contacts.id) as contacts_id, contacts.name as contacts_name
+        FROM contacts WHERE contacts.name LIKE :query ORDER BY contacts.name";
         $statement = $this->db->prepare($query);
         $statement->bindValue(':query', '%' . $searchQuery . '%');
         $statement->execute();
@@ -45,11 +47,13 @@ class Contact
         return $statement->fetch(\PDO::FETCH_NUM);
     }
 
-    public function getContactsLimitedPerPage($contactsPerPage, $offset, $searchQuery, $sortField = 'name' , $sortOrder = 'asc'){
+    public function getContactsLimitedPerPage($contactsPerPage, $offset, $searchQuery, $sortField = 'contacts.name' , $sortOrder = 'asc'){
         
-        $query = "SELECT id, name, company_id, email, phone, created_at, updated_at
+        $query = "SELECT contacts.id as contacts_id, contacts.name as contacts_name, contacts.company_id as contacts_company_id, contacts.email as contacts_email, contacts.phone as contacts_phone, contacts.created_at as contacts_created_at, contacts.updated_at as contacts_updated_at, companies.id as companies_id, companies.name as companies_name
         FROM contacts
-        WHERE name LIKE :query
+        INNER JOIN companies
+        ON contacts.company_id = companies.id
+        WHERE contacts.name LIKE :query
         ORDER BY $sortField $sortOrder
         LIMIT $contactsPerPage OFFSET $offset";
 
@@ -59,5 +63,4 @@ class Contact
 
         return $statement->fetchAll(\PDO::FETCH_ASSOC);
     }
-
 }
