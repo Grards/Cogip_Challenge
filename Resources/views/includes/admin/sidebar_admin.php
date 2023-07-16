c610764075991f3a23f2c0e7a19c233b9d4ab0de
BillyGrind <denayeranthony623@gmail.com>
2023-07-16 22:33:37 +0200

    add sidebar mobile first

--- Resources/views/includes/admin/sidebar_admin.php
+++ Resources/views/includes/admin/sidebar_admin.php
@@ -5,12 +5,36 @@
             <h3 class="user_card--first_name"><?php echo "$user[users_first_name]"; ?></h3>
             <h3 class="user_card--last_name"><?php echo "$user[users_last_name]"; ?></h3>
         </section>
+
         <section class="dash-sidebar__nav">
-            <p class="log--link"><a href="<?php echo BASE_URL.'dashboard'; ?>">Dashboard</a></p>
-            <p class="log--link"><a href="<?php echo BASE_URL.'dashboard/new-invoice'; ?>">Invoices</a></p>
-            <p class="log--link"><a href="<?php echo BASE_URL.'dashboard/new-company'; ?>">Companies</a></p>
-            <p class="log--link"><a href="<?php echo BASE_URL.'dashboard/new-contact'; ?>">Contacts</a></p>
+            <ul>
+                <li class="log--link">
+                    <a href="<?php echo BASE_URL.'dashboard'; ?>">
+                        <img src="assets\img\sidebar_logo\dashboard_logo.png" alt="dashboard logo">
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="log--link">
+                    <a href="<?php echo BASE_URL.'dashboard/new-invoice'; ?>">
+                        <img src="assets\img\sidebar_logo\invoices_logo.png" alt="">
+                        <span>Invoices</span>
+                    </a>
+                </li>
+                <li class="log--link">
+                    <a href="<?php echo BASE_URL.'dashboard/new-company'; ?>">
+                         <img src="assets/img/sidebar_logo/companies_logo.png" alt="logo company">
+                        <span>Companies</span>
+                    </a>
+                </li>
+                <li class="log--link">
+                    <a href="<?php echo BASE_URL.'dashboard/new-contact'; ?>">
+                        <img src="assets\img\sidebar_logo\contacts_logo.png" alt="">
+                        <span>Contacts</span
+                    ></a>
+                </li>
+            </ul>
         </section>
+
         <section class="dash-sidebar__log">
             <img class="log--picture" src="<?php echo IMG."contacts/$user[users_picture]"; ?>" alt="User <?php echo "$user[users_first_name]"; ?>'s profil picture">
             <!-- TODO LOGOUT SYSTEM -->
