95c1b6b7ac656f4406c15e06cbabed3e64d3d22e
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-07-18 11:28:54 +0200

    Add last invoice table for company details page

--- Resources/views/companyDetails.php
+++ Resources/views/companyDetails.php
@@ -17,42 +17,43 @@ include VIEWS . 'includes/dateFormat.php';
         <div id="test-clip">
         </div>
     </div>
-<div class="company-details">
-
-<?php
-if(isset($company)){
-    echo "<h1>$company[companies_name]</h1>";
-    echo "<p> Name : " . $company['companies_name'] . "</p>";
-    echo "<p> TVA : " . $company['tva'] . "</p>";
-    echo "<p> Country : " . $company['country'] . "</p>";
-    echo "<p> Type : " . $company['types_name'] . "</p>";
-} else {
-    echo "<p>Company details not found.</p>";
-}
-?>
-</div>
-<div class="contact-people-picture">
-<h2>Contact people</h2>
-<?php
-// TODO: Add picture of the contacts working in this company
-// echo "<img class='contact-picture' src='" . IMG."contacts/$contact[contacts_picture]' alt=' . $contact[contacts_name] .'s picture>";
-// echo "<img class='contact-picture' src='" . IMG."contacts/$contact[contacts_picture]' alt=' . $contact[contacts_name] .'s picture>";
-?>
-</div>
-
-<div class="table-container top">
-      <div class="table-title">
-          <h2>Last invoices</h2>
-      </div>
+    <div class="company-details">
+    <div class="table-title">
+            <h2>Contact people</h2>
+        </div>
+        <?php
+        if (isset($company)) {
+            echo "<h1>$company[companies_name]</h1>";
+            echo "<p> Name : " . $company['companies_name'] . "</p>";
+            echo "<p> TVA : " . $company['tva'] . "</p>";
+            echo "<p> Country : " . $company['country'] . "</p>";
+            echo "<p> Type : " . $company['types_name'] . "</p>";
+        } else {
+            echo "<p>Company details not found.</p>";
+        }
+        ?>
+    </div>
+    <div class="contact-people-picture">
+        <?php
+        // TODO: Add picture of the contacts working in this company
+        // echo "<img class='contact-picture' src='" . IMG."contacts/$contact[contacts_picture]' alt=' . $contact[contacts_name] .'s picture>";
+        // echo "<img class='contact-picture' src='" . IMG."contacts/$contact[contacts_picture]' alt=' . $contact[contacts_name] .'s picture>";
+        ?>
+    </div>
+    <div class="table-container">
+        <div class="table-title">
+            <h2>Last invoices</h2>
+        </div>
         <table class="table">
-          <thead class="tableHead">
-            <th>Invoice Number</th>
-            <th>Dates due</th>
-            <th>Company</th>
-            <th>Created at</th>
-          </thead>
-
-          <!-- TODO : Add last invoices of this specific company -->
+            <thead class="tableHead">
+                <th>Invoice Number</th>
+                <th>Dates</th>
+                <th>Company</th>
+                <th>Created at</th>
+            </thead>
+        </table>
+    </div>
+            <!-- TODO : Add last invoices of this specific company -->
 
 </main>
 
