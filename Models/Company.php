50f24d43f52e65e9c97e263ded0914e7b146a3e2
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-07-18 11:01:26 +0200

    Add company details page

--- Models/Company.php
+++ Models/Company.php
@@ -13,6 +13,19 @@ class Company
         $this->db = DatabaseManager::getInstance();
     }
 
+    public function getCompanyById($id){
+
+        $query = "SELECT companies.id as companies_id, companies.name as companies_name, companies.type_id as companies_type_id, types.id as types_id, types.name as types_name, country, tva
+        FROM companies
+        INNER JOIN types ON companies.type_id = types.id
+        WHERE companies.id = :id";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch(\PDO::FETCH_ASSOC);
+    }
+
     public function getLatestCompanies($limit)
     {
         $query = "SELECT companies.id as companies_id, companies.name as companies_name, companies.type_id as companies_type_id, companies.country as companies_country, companies.tva as companies_tva, companies.created_at as companies_created_at, companies.updated_at as companies_updated_at, types.id as types_id, types.name as types_name 
