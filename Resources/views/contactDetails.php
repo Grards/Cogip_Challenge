a6f688ba7e8f1b7c86193a2b95ec9fd95f980418
Anthony <denayeranthony1782@gmail.com>
2023-07-18 11:26:53 +0200

    add yellow submarine in title

--- Resources/views/contactDetails.php
+++ Resources/views/contactDetails.php
@@ -7,23 +7,21 @@ include VIEWS . 'includes/errors.php';
 include VIEWS . 'includes/dateFormat.php';
 
 ?>
-
 <main>
     <div class="table-shape">
         <div class="test-clip">
         </div>
     </div>
-    <div class="table-container withoutSlogan">
-        <div id="test-clip">
-        </div>
-    </div>
     <?php
     if(isset($contact)){
-    echo "<h1>$contact[contacts_name]</h1>";
+    echo"<div class='detail-container'>";
+    echo "<h2>$contact[contacts_name]</h2>";
+    echo"<img src='./public/assets/img/background-title.png' alt='background yellow title'>";
     echo "<p>Contact : " . $contact['contacts_name'] . "</p>";
     echo "<p>Phone : " . $contact['contacts_phone'] . "</p>";
     echo "<p>Mail : " . $contact['contacts_email'] . "</p>";
     echo "<p>Company : " . $contact['companies_name'] ."</p>";
+    echo "</div>"; 
     } else {
         echo "Contact details not found.";
     }
